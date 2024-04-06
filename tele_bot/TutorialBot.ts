b99af36e3f8fa52c5e4c5251db0639dc37376ab7
Hui Yao <leehuiyao@outlook.com>
2024-04-06 13:37:56 +0800

    Working Menu with dynamic text

--- tele_bot/TutorialBot.ts
+++ tele_bot/TutorialBot.ts
@@ -7,95 +7,78 @@ interface BotConfig {
     targetLanguage: String;
 }
 
+//Create config
+const botConfig: BotConfig = {
+    baseLanguage: 'English',
+    targetLanguage: 'Spanish',
+};
+
+
 // Create an instance of the `Bot` class and pass your bot token to it.
 const bot = new Bot("6991582486:AAGVKAI23eL1dKKZ-Lv-S3o3KGcKh_tSIP4"); // <-- put your bot token between the ""
+// Pre-assign menu text
+const mainMenuText = () => {
+    return `<b>Translation Bot menu </b>
 
-// //Pre-assign menu text
-const mainMenuText = "<b>Translation Bot menu</b>\n\n You can change the language settings here\n";
-const languageMenuText = "<b>Translation Bot menu</b>\n\n Select a language!"
 
+    Base Language: ${botConfig.baseLanguage}
+    Target Language: ${botConfig.targetLanguage}
+    
+    You can change the language settings here`;
+}
+
+
+//set the languages
+const languages = ["English", "Chinese", "Malay", "Thai"];
+
+//Define main menu
 const mainMenu = new Menu('main-menu-identififer')
-    .submenu("Select Base Language", 'language-menu')
-    .submenu("Select Target Language", 'language-menu');
+    .submenu("Select Base Language", 'language-menu-base')
+    .submenu("Select Target Language", 'language-menu-target');
+
+//Define language submenu
+const createLanguageMenu = (type: 'base' | 'target') => {
+    //Set target or base language
+    const languageMenu = new Menu(`language-menu-${type}`);
+
+    languages.forEach((language) => {
+        languageMenu.text(language, async (ctx) => {
+
+            if (type === 'base') {
+                botConfig.baseLanguage = language;
+            }
+            else if (type == 'target') {
+                botConfig.targetLanguage = language;
+            }
+
+            await ctx.reply(`${type === 'base' ? 'Base' : 'Target'} language set to ${language}`);
+            await ctx.editMessageText(mainMenuText(), { reply_markup: mainMenu });
 
 
+        });
+    });
 
-const languageSettings = new Menu('language-menu')
-    .text("English", (ctx) => ctx.reply("English"))
-    .text("Chinese", (ctx) => ctx.reply("Chinese"))
-    .text("Malay", (ctx) => ctx.reply("Malay"))
-    .text("Thai", (ctx) => ctx.reply("Thai"))
-    .back("Go Back");
+    languageMenu.back("Go Back");
 
-//Register settings menu at main menu.
-mainMenu.register(languageSettings);
+    return languageMenu;
+};
 
+//Create  menu objects
+const baseLanguageMenu = createLanguageMenu('base');
+const targetLanguageMenu = createLanguageMenu('target');
+
+// Register settings menus at main menu
+mainMenu.register(baseLanguageMenu);
+mainMenu.register(targetLanguageMenu);
+
+//Bot to use main menu
 bot.use(mainMenu);
 
+//Handle the command /menu
 bot.command("menu", async (ctx) => {
-    // Send the menu.
-    await ctx.reply(mainMenuText, { reply_markup: mainMenu });
+    // Send the menu
+    await ctx.reply(mainMenuText(), { reply_markup: mainMenu });
 });
-
-
-// //Pre assigned button 
-// const baseLanguage = "Base Language";
-// const targetLanguage = "Target Language";
-
-// //Set language
-// const languagePairs = [
-//     ["English", "english"],
-//     ["Chinese", "chinese"],
-//     ["Malay", "malay"],
-//     ["Thai", "thai"],
-// ]
-
-// //Building menu 
-// const mainMenu = new Menu(mainMenuText)
-//     .text(baseLanguage, 'select-base-language')
-//     .text(targetLanguage, 'select-target-language');
-
-// //Build keyboards
-// const menuMarkup = new InlineKeyboard()
-//     .text(baseLanguage, 'select-base-language')
-//     .text(targetLanguage, 'select-target-language');
-
-// const languageListRow = languagePairs
-//     .map(([label, data]) => InlineKeyboard.text(label, data));
-
-// const languageListMarkup = languageListRow;
-// ;
-
-// // Handle the /start command.
-// bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-
-// //handle the /menu command 
-// //This handler sends a menu with the inline buttons we pre - assigned above
-// bot.command("menu", async (ctx) => {
-//     await ctx.reply(mainMenu, {
-//         parse_mode: "HTML",
-//         reply_markup: menuMarkup,
-//     });
-// });
-
-// // handle the call back for select-base-language
-// bot.callbackQuery("select-base-language", async (ctx) => {
-//     //Update the message content with the corresponding menu section 
-//     await ctx.answerCallbackQuery(
-//         const main = new
-//     )
-// });
-
-
-
-// bot.callbackQuery('select-target-language', async (ctx) => {
-//     //Update the message content with the corresponding menu section
-//     await ctx.editMessageText(languageMenu, {
-//         reply_markup: languageListMarkup,
-//         parse_mode: "HTML",
-//     })
-// });
-
 
 //handle any messages
 bot.on("message", async (ctx) => {
